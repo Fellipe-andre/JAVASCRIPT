7e61cbe5d30a4b2d0f81ed543fc2803947bad61d
fellipe-andre <fellipeandre.usa@gmail.com>
2022-11-21 09:44:31 -0300

    up

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,7 @@
+num = [5,8,2,9,3]
+num[3]= 6 
+num.push(4)
+console.log(`O aray tem ${num.length} posições`)
+num.sort()// serve para colocar os numeros dentro do array em ordem crecente
+console.log(`Nosso vetor(array) é o ${num}`)
+console.log(`O primeiro valor é ${num[0]}`)//sempre começa pelo 0
