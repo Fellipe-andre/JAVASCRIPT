ca861ed6a28de83f58e32e826a6da79cc4fe9ec6
Fellipe Moraes <113467346+Fellipe-andre@users.noreply.github.com>
2023-06-12 14:41:07 -0300

    up

--- dev samurai/arrays/filter.js
+++ dev samurai/arrays/filter.js
@@ -1,4 +1,7 @@
-/*var produtos = {'Esporte':[{id:1, nome: 'bola', cor: 'verde', tamanho: 'unico',}]};
+/*
+-----------------------Filter------------------------------------
+
+var produtos = {'Esporte':[{id:1, nome: 'bola', cor: 'verde', tamanho: 'unico',}]};
 
 
 //usando filter com numeros
@@ -38,10 +41,42 @@ var numerosImpares = numeros.filter(impares);
 
 -----/----------------------------------------------
 
-//usando filter com arrays de objetos*/
+//usando filter com arrays de objetos
 
 var filmes = [
     {titulo: 'Titanic', Duração: 203, nota: 7.5},
-    {titulo: 'Titanic', Duração: 203, nota: 9.5},
-    {titulo: 'Titanic', Duração: 203, nota: 6.5},
-]
+    {titulo: 'Avengers', Duração: 197, nota: 9.5},
+    {titulo: 'Mr. Bean', Duração: 90, nota: 6.5},
+];
+
+var notaCorte = 8;
+
+
+var bons = function(item){
+    return item.nota > notaCorte;
+}
+
+var ruins = function(item){
+    return item.nota < notaCorte;
+}
+
+var filmesBons = filmes.filter(bons);
+var filmesRuins = filmes.filter(ruins);
+
+console.log(filmesBons);
+console.log(filmesRuins);
+
+
+var convidados = [
+    {nome: "Fellipe", vip: true, idade: 32},
+    {nome: "Vitor", vip: true, idade: 30},
+    {nome: "Thiago", vip: false, idade: 34},
+    {nome: "Paulo", vip: false, idade: 65}
+];
+
+var vip = convidados.filter(function(item){
+    return item.vip;
+});
+
+console.log(vip);
+*/
